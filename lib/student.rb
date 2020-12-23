f1969d415f2567d7f6661f1f2919b88f00ee6354
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 22:10:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL 
+    sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
 
@@ -58,7 +58,7 @@ class Student
         self.new_from_db(row)
       end.first
     end
-    
+
   end
 
 end
