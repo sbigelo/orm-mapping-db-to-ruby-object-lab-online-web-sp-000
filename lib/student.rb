5793257ffb9aa63547dbc75ed12ef7934f7d50b1
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 22:13:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     SELECT * FROM students
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
     end
   end
