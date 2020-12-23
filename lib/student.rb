1e42e7c6d37a16c597fad12c28db14261380a68d
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 22:22:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,7 @@ class Student
 
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 12
+    SELECT x FROM students WHERE grade = 12
     SQL
     DB[:conn].execute(sql, x).map do |row|
         self.new_from_db(row)
