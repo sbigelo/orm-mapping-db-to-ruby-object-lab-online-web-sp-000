d835f33a45911d81187811050ddc09c71d828589
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-23 22:15:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,9 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 9
     SQL
-
+    DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+    end
   end
 
 end
